ff5bb1ebc6800912290a3cb773a3f864122a32bb
Mina Mikhail <mina@fightingtheboss.com>
2014-02-21 17:26:06 -0500

    Add index view for votes_controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,20 @@
 class VotesController < ApplicationController
+  before_filter :ensure_logged_in
+
+  def index
+    @vote = Vote.new
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  private
+  def ensure_logged_in
+    redirect_to root_url unless current_user
+  end
 end
