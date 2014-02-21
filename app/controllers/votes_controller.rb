dbc03037ca96a525506b952af80196b2bcafb86b
Mina Mikhail <mina@fightingtheboss.com>
2014-02-21 18:29:50 -0500

    Setup the controller to return JSON response

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -6,14 +6,27 @@ class VotesController < ApplicationController
   end
 
   def create
+    @vote = Vote.new(vote_params)
+    @vote.user = current_user
 
-  end
+    if @vote.save
+      respond_to do |format|
+        format.html { redirect_to votes_url }
+        format.json do
+          render :json => @vote.to_json
+        end
+      end
 
-  def show
-
+    else
+      render :index
+    end
   end
 
   private
+  def vote_params
+    params.require(:vote).permit(:response)
+  end
+
   def ensure_logged_in
     redirect_to root_url unless current_user
   end
