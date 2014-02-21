18e2d299c80c08112803107b38e18f17302f3ef0
Ryan Ming <ryan.ming@gmail.com>
2014-02-21 16:38:37 -0500

    require bcrypt engine (why is this necessary?), remove attr_accessor because reasons

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  attr_accessor :email, :password, :password_confirmation
+  # attr_accessor :email, :password, :password_confirmation
 
   validates_uniqueness_of :email
 end
